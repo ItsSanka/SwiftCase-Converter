6cd70b5b2f9ef7cd393f8fe246659ab703b6ab38
sanka <itssanka97@gmail.com>
2024-02-04 12:56:50 +0530

    capitalizecase

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,29 @@
 let a;
 
-document.getElementById("upperCase").onclick = function(){
+document.getElementById("upperCase").onclick = function () {
     a = document.getElementById("label").value;
     a = a.toUpperCase();
 
     document.getElementById("label").value = a;
 }
 
-document.getElementById("lowCase").onclick = function(){
+document.getElementById("lowCase").onclick = function () {
     a = document.getElementById("label").value;
     a = a.toLowerCase();
 
     document.getElementById("label").value = a;
 }
 
-document.getElementById("clear").onclick = function(){
+document.getElementById("CapitalCase").onclick = function () {
     a = document.getElementById("label").value;
-    a = a.value =""
+    a = a.charAt(0).toUpperCase() + a.slice(1, a.indexOf(" "));
+
+    document.getElementById("label").value = a;
+}
+
+document.getElementById("clear").onclick = function () {
+    a = document.getElementById("label").value;
+    a = a.value = ""
 
     document.getElementById("label").value = a;
 }
